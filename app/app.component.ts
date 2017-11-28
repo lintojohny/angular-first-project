d0e84e796cde8a3a24a2d8583939bbfc3972e9e1
linto <johnjohnythayyil@gmail.com>
2017-11-28 10:17:20 +0530

    coustom pipes

---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+
+
+
+@Component({
+    selector: 'my-app',
+    template: `   
+    	<ul class= "nav nav-pills">
+    		<li [class.active]="viewMode == 'map'"> <a (click)="viewMode= 'map'"> Map View </a></li> 
+    		<li [class.active]="viewMode == 'list'"> <a (click)="viewMode= 'list'"> list View </a></li> 
+    	</ul>
+    	<div [ngSwitch]= "viewMode"> 
+    		<template [ngSwitchWhen]="'map'" ngSwitchDefault> Map View Content </template>
+    		<template [ngSwitchWhen]="'list'" ngSwitchDefault> list View Content </template>
+
+    	</div>
+    `
+}) 
+export class AppComponent { 
+	viewMode = 'map';
+
+}
